d5ee000bfc25c23e34940e54be26518615d10ebc
Joaquin Rapela <joacorapela@gmail.com>
2021-05-13 21:49:02 +0100

    Added function plot_1D_data

--- code/scripts/myFirstScript.py
+++ code/scripts/myFirstScript.py
@@ -1,10 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def plot_1D_data(x, y, xlabel="x", ylabel="y", title="My First Plot",
+                 line_color="red", line_style="dotted"):
+    plt.plot(x, y, color=line_color, linestyle=line_style)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+
 x = np.genfromtxt("../../data/x.csv")
 y = np.genfromtxt("../../data/y.csv")
 
-plt.plot(x, y)
+plot_1D_data(x, y)
 plt.show()
+plt.savefig("../../figures/3_exercise1_default_params.png")
 
-plt.savefig("../../figures/1_exercise2.png")
+plot_1D_data(x, y, xlabel="data", ylabel="predictions",
+             title="Plot with custom parameters", line_color="blue",
+             line_style="dashed")
+plt.show()
+plt.savefig("../../figures/3_exercise1_custom_params.png")
+
