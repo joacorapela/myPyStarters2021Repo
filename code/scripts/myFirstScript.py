23e50bcc607d1ea1d9fb00bd86a95c3981fc8a74
Joaquin Rapela <joacorapela@gmail.com>
2021-05-13 21:29:32 +0100

    Added

---
+++ code/scripts/myFirstScript.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.genfromtxt("../../data/x.csv")
+y = np.genfromtxt("../../data/y.csv")
+
+plt.plot(x, y)
+plt.show()
+
+plt.savefig("../../figures/1_exercise2.png")
