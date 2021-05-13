46c7e4d06dd3d11e9e78b91e52d935914c9f91be
Joaquin Rapela <joacorapela@gmail.com>
2021-05-13 22:27:14 +0100

    Fixed problem with saving figures in 1st version

--- code/scripts/myFirstScript.py
+++ code/scripts/myFirstScript.py
@@ -1,23 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_1D_data(x, y, xlabel="x", ylabel="y", title="My First Plot",
-                 line_color="red", line_style="dotted"):
-    plt.plot(x, y, color=line_color, linestyle=line_style)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.title(title)
-
 x = np.genfromtxt("../../data/x.csv")
 y = np.genfromtxt("../../data/y.csv")
 
-plot_1D_data(x, y)
-plt.savefig("../../figures/3_exercise1_default_params.png")
+plt.plot(x, y)
+plt.savefig("../../figures/1_exercise2.png")
 plt.show()
-
-plot_1D_data(x, y, xlabel="data", ylabel="predictions",
-             title="Plot with custom parameters", line_color="blue",
-             line_style="dashed")
-plt.savefig("../../figures/3_exercise1_custom_params.png")
-plt.show()
-
